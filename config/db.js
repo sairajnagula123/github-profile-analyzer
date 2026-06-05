598c929e5fe60f0fc16ce8d5393321029ce59e82
Sairaj <nagulasairaj16@gmail.com>
2026-06-05 15:20:29 +0530

    Railway database integration completed

--- config/db.js
+++ config/db.js
@@ -4,12 +4,13 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log("Database Error", err);
+    console.log("Database Error:", err);
   } else {
     console.log("MySQL Connected");
   }
