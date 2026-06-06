dd3ccf6f8ea76aed1f4835572914f835ab9b3d47
Sairaj <nagulasairaj16@gmail.com>
2026-06-06 21:05:35 +0530

    Add root route

--- services/githubService.js
+++ services/githubService.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
 
 const fetchGithubProfile = async (username) => {
-
   const response = await axios.get(
-    `https://api.github.com/users/${username}`
+    `https://api.github.com/users/${username}`,
+    {
+      headers: {
+        "User-Agent": "github-profile-analyzer"
+      }
+    }
   );
 
   return response.data;
