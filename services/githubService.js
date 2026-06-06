faa9dc29c7f1c0633a060350f7029d4eb06041f2
Sairaj <nagulasairaj16@gmail.com>
2026-06-06 21:11:55 +0530

    Add GitHub token authentication

--- services/githubService.js
+++ services/githubService.js
@@ -5,6 +5,7 @@ const fetchGithubProfile = async (username) => {
     `https://api.github.com/users/${username}`,
     {
       headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
         "User-Agent": "github-profile-analyzer"
       }
     }
